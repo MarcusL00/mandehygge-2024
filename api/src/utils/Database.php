5a259c210d4d862bf2bd277e704c56dea28101af
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-07-12 19:18:47 +0200

    added exit on database failure

--- api/src/utils/Database.php
+++ api/src/utils/Database.php
@@ -49,6 +49,7 @@ class Database
         } catch (\PDOException $e) {
             http_response_code(500);
             echo json_encode(["success" => false]);
+            exit;
         }
     }
 }
